1ae63518d2707e91f2cbb1424ef0066d1aae63ea
anonymous anonymous <lauralpeng@gmail.com>
2022-03-10 20:48:52 +0000

    Generate ingredients resource with ideas:scaffold

---
+++ app/models/ingredient.rb
@@ -0,0 +1,14 @@
+class Ingredient < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    ingredient_name
+  end
+
+end
---
+++ app/views/ingredients/_ingredient.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! ingredient, :id, :ingredient_name, :have_vs_not, :created_at, :updated_at
+json.url ingredient_url(ingredient, format: :json)
