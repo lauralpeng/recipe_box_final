56f08ed028ad8a815df9bbe40092f78a3d4e7363
anonymous anonymous <lauralpeng@gmail.com>
2022-03-10 20:49:57 +0000

    Generate graphiti resources for dishes

---
+++ app/resources/dish_resource.rb
@@ -0,0 +1,17 @@
+class DishResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :dish_name, :string
+  attribute :source, :string
+  attribute :time_required, :integer
+  attribute :category_id, :integer
+  attribute :user_id, :integer
+  attribute :dish_photo, :string
+  attribute :instructions, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
