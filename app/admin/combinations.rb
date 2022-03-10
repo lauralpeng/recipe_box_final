04432074a8a6d87acccfad7e91bfa324ed551734
anonymous anonymous <lauralpeng@gmail.com>
2022-03-10 20:49:17 +0000

    Whitelist columns in Combination for ActiveAdmin

--- app/admin/combinations.rb
+++ app/admin/combinations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Combination do
 
+ permit_params :ingredient_id, :quantity, :unit, :dish_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
