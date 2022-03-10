af216d186ab8498459d4171b2972f8209923595e
anonymous anonymous <lauralpeng@gmail.com>
2022-03-10 20:49:21 +0000

    Whitelist columns in Ingredient for ActiveAdmin

--- app/admin/ingredients.rb
+++ app/admin/ingredients.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Ingredient do
 
+ permit_params :ingredient_name, :have_vs_not
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
