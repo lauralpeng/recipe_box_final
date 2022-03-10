f287175199394c3831a48248e865eaae09bf9029
anonymous anonymous <lauralpeng@gmail.com>
2022-03-10 20:50:05 +0000

    Generate graphiti resources for ingredients

---
+++ app/resources/ingredient_resource.rb
@@ -0,0 +1,12 @@
+class IngredientResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :ingredient_name, :string
+  attribute :have_vs_not, :boolean
+
+  # Direct associations
+
+  # Indirect associations
+
+end
